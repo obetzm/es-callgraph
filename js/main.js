6065d6044477bef123f3d6796076961bc256d675
Matthew Obetz <obetzm@rpi.edu>
2018-11-01 16:52:55 -0400

    wip ast walking -- moving to laptop for office hours

--- js/main.js
+++ js/main.js
@@ -1,37 +1,27 @@
 "use strict";
 
-let parser = require("esprima");
-let yaml = require("js-yaml");
 let fs = require("fs");
-let process_serverless = require("./serverless_yml_processing");
-let {CallGraph} = require("./call_graph");
+let parser = require("esprima");
+let {process_serverless, find_serverless_files, load_yaml_from_filename} = require("./serverless_yml_processing");
+let {CallGraph,GraphNode,GraphEdge} = require("./call_graph");
 
 
-function parse_lambda(entry_file, main_method, ancestor_node) {
-    let ast = parser.parseScript(entry_file);
+function walk_ast(entry_file, main_method, entry_node, ancestor) {
+    let ast = parser.parseModule(entry_file);
     let new_nodes = [];
 
     ast.body.forEach((stmt) => {
-        console.log(stmt);
+        console.log(JSON.stringify(stmt, null, 2));
+        if (stmt.type === "FunctionExpression" && id.name !== undefined && id.name !== null) {
+            let found_method = new GraphNode("lambda", main_method+"."+id.name);
+            new_nodes.push(found_method);
+            walk_ast(stmt.body, main_method, entry_node, found_method);
+        }
+        //look for pattern of call to db function
     });
 
-    return [] //new things that need processing
+    return new_nodes //nodes updated by this parse
 }
-
-let load_yaml_from_filename = (filename) => [filename, yaml.safeLoad(fs.readFileSync(filename, 'utf8'))];
-
-let find_serverless_files = (paths, known) => {
-    known = known === undefined ? [] : known;
-    let path_contents = paths
-        .reduce((all_paths, path)=> all_paths.concat(fs.readdirSync(path).map((file)=>path+"/"+file)), []);
-
-    let found_serverless = known.concat(path_contents.filter((p)=>p.endsWith("serverless.yml")));
-    let subdirs = path_contents.filter((p)=>fs.statSync(p).isDirectory());
-
-    if ( subdirs.length === 0 )
-        return found_serverless;
-    return find_serverless_files(subdirs, found_serverless);
-};
 
 function main(directories) {
     let files = find_serverless_files(directories);
@@ -41,18 +31,20 @@ function main(directories) {
         .map(([f, g]) => g.set_context_on_nodes(f))
         .reduce((a,n) => a.union_graphs(n), new CallGraph());
 
-    let worklist = [...graph.nodes].filter((n) => n.type === "lambda");
+    let initial_files = [...graph.nodes].filter((n) => n.type === "lambda");
 
 
-    while (worklist.length > 0 ) {
-        let next_method = worklist.pop(0);
+    while (initial_files.length > 0 ) {
+        let next_method = initial_files.shift();
         let lambda_filename = next_method.context.slice(0, next_method.context.lastIndexOf("/")) + "/" + next_method.label.slice(0, next_method.label.indexOf(".")) + ".js";
         let lambda_entrypoint = next_method.label.slice(next_method.label.indexOf(".") + 1);
 
         let entry_file = fs.readFileSync(lambda_filename, 'utf8');
-        parse_lambda(entry_file, lambda_entrypoint, next_method);
-    }//while the worklist is not empty
+        walk_ast(entry_file, lambda_entrypoint, next_method, null);
+    }//while we have files to process
 
 }//main
 
-main(process.argv.slice(2));
+if (require.main === module) {
+    main(process.argv.slice(2));
+}
--- js/serverless_yml_processing.js
+++ js/serverless_yml_processing.js
@@ -1,10 +1,8 @@
 "use strict";
 
 let {GraphNode, GraphEdge, CallGraph} = require("./call_graph");
-
-
-
-
+let yaml = require("js-yaml");
+let fs = require("fs");
 
 
 const event_label_map = Object.freeze({
@@ -30,10 +28,37 @@ function config_function_to_graph(f) {
         .reduce((g, n) => g.add_node(n.node).add_edge(n.edge), new CallGraph().add_node(lambda_node));
 }
 
-module.exports = function convert_serverless_to_graph(config) {
-    if (config.functions === undefined) { config.functions=[]; }
+
+let process_serverless = function (config) {
+    if (config.functions === undefined) {
+        config.functions = [];
+    }
     return Object.keys(config.functions)
         .map((f) => config.functions[f])
         .map(config_function_to_graph)
         .reduce((a, n) => a.union_graphs(n), new CallGraph());
+};
+
+
+let load_yaml_from_filename = (filename) => [filename, yaml.safeLoad(fs.readFileSync(filename, 'utf8'))];
+
+
+let find_serverless_files = (paths, known) => {
+    known = known === undefined ? [] : known;
+    let path_contents = paths
+        .reduce((all_paths, path) => all_paths.concat(fs.readdirSync(path).map((file) => path + "/" + file)), []);
+
+    let found_serverless = known.concat(path_contents.filter((p) => p.endsWith("serverless.yml")));
+    let subdirs = path_contents.filter((p) => fs.statSync(p).isDirectory());
+
+    if (subdirs.length === 0)
+        return found_serverless;
+    return find_serverless_files(subdirs, found_serverless);
+};
+
+
+module.exports = {
+    "process_serverless": process_serverless,
+    "load_yaml_from_filename": load_yaml_from_filename,
+    "find_serverless_files": find_serverless_files
 };
